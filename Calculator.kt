f550f50bda6b23fd767a14d2a29f89e15cd0946f
sumayaalqadhy <sumaya.alqadhy@gmail.com>
2024-09-04 20:04:49 -0700

    Added calculator's functionalities

--- Calculator.kt
+++ Calculator.kt
@@ -5,4 +5,32 @@ class Calculator (val name: String) {
   init {
     println("$name's calculator")
   }
+
+  fun add(a: Int, b: Int): Int {
+    return a + b
+  }
+
+  fun subtract(a: Int, b: Int): Int {
+    return a - b
+  }
+
+  fun multiply(a: Int, b: Int): Int {
+    return a * b
+  }
+
+  fun divide(a: Int, b: Int): Any {
+    if (b == 0) {
+      return "Error!"
+    } else {
+      return a / b
+    }
+  }
+
+  fun power(a: Int, b: Int): Int {
+    var result = 1
+    for (i in 1..a) {
+      result = result * b
+    }
+    return result
+  }
 }
